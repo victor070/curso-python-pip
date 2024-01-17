ded01f8a840c28a889fc95fcf06bdb02477fbac7
Victor Corado <jcorado@habitatguate.org>
2024-01-17 15:39:34 -0600

    web service list , js and html

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,17 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return "<h1>hola</h1>"
+    
 
 def run():
     store.get_categories()
